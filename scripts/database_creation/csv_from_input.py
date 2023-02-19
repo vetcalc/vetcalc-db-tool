2ef31f0e7f89be0b42186f75fa282522e61ded54
Liam <mailman1276@gmail.com>
2023-02-18 23:04:23 -0800

    removed unneeded dedup functions

--- scripts/database_creation/csv_from_input.py
+++ scripts/database_creation/csv_from_input.py
@@ -3,14 +3,11 @@ import table_writing as tw
 import deduplication as ddp
 import make_objects as mo
 import id_replacement as ri
-import print_debug as pd
 
 def main():
     entities = mo.make_entities()
-    ddp.dedup_entities(entities)
-    ri.replace_with_ids(entities)
-    pd.show_entities(entities)
-    tw.write_tables_as_csv(entities)
+    # ri.replace_with_ids(entities)
+    # tw.write_tables_as_csv(entities)
 
 if __name__ == "__main__":
     main()
