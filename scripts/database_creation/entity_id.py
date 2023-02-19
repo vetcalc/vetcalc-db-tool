960c838f6f576a6ffde724fe46977f7f13823001
Liam <mailman1276@gmail.com>
2023-02-18 23:02:06 -0800

    updating entity ids to be hashable and incorporate type annotations. Moved strip_prefix into a class method.

--- scripts/database_creation/entity_id.py
+++ scripts/database_creation/entity_id.py
@@ -3,10 +3,10 @@ class EntityId:
     EntityIds will be represented as a prefix plus some number.
     This number can be whatever it needs to be, e.g., a sequence, UUID, or ULID.
     The prefix is their for debugging purposes to aid in quickly finding the
-    table for search for a given entity.
+    table for search for a given id.
     '''
 
-    def __init__(self, prefix, id):
+    def __init__(self, prefix: str, id: int):
         self.id = id
         self.prefix = prefix
 
@@ -18,16 +18,18 @@ class EntityId:
     def __eq__(self, other):
         return self.get() == other.get()
 
+    
+    def __hash__(self):
+        return hash((self.id, self.prefix))
 
-    def show(self):
-        print (f"{self.prefix}_{self.id}")
 
-    def get(self):
-        return f"{self.prefix}_{self.id}"
+    def get(self, no_prefix=False) -> int | str:
+        if no_prefix:
+            end_of_prefix = id.get().index("_")
+            return int(id.get()[end_of_prefix+1:])
+        else:
+            return f"{self.prefix}_{self.id}"
 
-    def set(self, id):
+    def set(self, id: int):
         self.id = id
 
-def strip_prefix(id):
-    end_of_prefix = id.index("_")
-    return id[end_of_prefix+1:]
